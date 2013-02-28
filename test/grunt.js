74d3ebe1584357293c5a29b94f672d44568c23af
Todd Wolfson <todd@twolfson.com>
2013-02-27 23:53:19 -0800

    Broke up grunt.js to stop confusing one-offs

--- test/grunt.js
+++ test/grunt.js
@@ -1,72 +1,13 @@
 module.exports = function (grunt) {
+  // Load in common config
+  grunt = require('./grunt.common.js')(grunt);
 
-  // Load in grunt-retro
-  grunt.loadTasks('../tasks');
-
-  // Project configuration.
-  grunt.initConfig({
-    'echo-src': {
-      'actual/src_compact.txt': 'test_files/file.js',
-      single: {
-        src: 'test_files/file.js',
-        dest: 'actual/src_single.txt'
-      },
-      multi: {
-        src: ['test_files/file.js', 'test_files/file2.js'],
-        dest: 'actual/src_multi.txt'
-      },
-      expansion: {
-        src: ['test_files/{file,file2}.js'],
-        dest: 'actual/src_expansion.txt'
-      },
-      // TODO: Move this to a string... =_=
-      uri: {
-        src: ['http://google.com/'],
-        dest: 'actual/src_uri.txt'
-      }
-    },
-    'echo-dest': {
-      'actual/compact_test.txt': 'actual/dest_compact.txt',
-      simple: {
-        src: 'actual/dest_simple.txt',
-        dest: 'actual/simple_test.js'
-      }
-    },
-    nodeunit: {
-      all: '*_test.js'
-    }
-  });
-  //   test: {
-  //     all: '*_test.js'
-  //   }
-  // });
-  // // TODO: Come back to this when less tired
-  // grunt.config.set('nodeunit', grunt.config.get('test'));
-
-  // Load in grunt-contrib-nodeunit
-  process.chdir('..');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  process.chdir(__dirname);
-
-  // Register task for testing src
-  grunt.registerMultiTask('echo-src', 'Save src to dest file', function () {
-    var file = this.file,
-        src = file.src,
-        dest = this.data.dest || this.target;
-    grunt.file.write(dest, JSON.stringify(src));
-  });
-
-  // Register task for testing dest
-  grunt.registerMultiTask('echo-dest', 'Save dest to src file', function () {
-    var file = this.file,
-        dest = file.dest,
-        src = this.data.src || this.data;
-    grunt.file.write(src, JSON.stringify(dest));
+  // Set up test config
+  grunt.config.set('test', {
+    all: '*_test.js'
   });
 
   // Run project task then tests.
   // TEST: We can actually run single string of queries
-  // TODO: Come back to this when less tired (for nodeunit)
-  grunt.registerTask('default', 'echo-src echo-dest nodeunit');
-  // grunt.registerTask('default', 'echo-src echo-dest test');
+  grunt.registerTask('default', 'test-setup test');
 };
