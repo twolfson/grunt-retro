dc415a836085c1683754930dde50c266ce760f62
Todd Wolfson <todd@twolfson.com>
2013-03-03 23:13:06 -0800

    Pulled back temporary changes for src templating

--- test/Gruntfile.js
+++ test/Gruntfile.js
@@ -1,14 +1,22 @@
 module.exports = function (grunt) {
-  // Load in grunt-retro
-  grunt = require('../tasks/retro')(grunt);
+  // // Load in grunt-retro
+  // grunt = require('../tasks/retro')(grunt);
 
-  // Load in common config
-  grunt = require('./grunt.common.js')(grunt);
+  // // Load in common config
+  // grunt = require('./grunt.common.js')(grunt);
 
   // Set up grunt 0.4 specific tests
-  var _ = grunt.utils._,
+  // var _ = grunt.utils._,
+  var _ = grunt.util._,
       gruntConfig = grunt.config,
-      echoDest = gruntConfig.get('echo-dest');
+      // echoDest = gruntConfig.get('echo-dest');
+      echoDest = {};
+  grunt.initConfig({
+    'pkg': require('../package.json')
+  });
+  gruntConfig.set('echo-src', _.extend(echoDest, {
+    'actual/src_template.txt': '<%= pkg.name %>.js'
+  }));
   gruntConfig.set('echo-dest', _.extend(echoDest, {
     '<%= pkg.name %>.js': 'actual/dest_template.txt'
   }));
@@ -27,8 +35,25 @@ module.exports = function (grunt) {
   // // Set up 0.4 tasks
   // grunt.registerTask('0.4-test', '');
 
+  // Register task for testing src
+  grunt.registerMultiTask('echo-src', 'Save src to dest file', function () {
+    var file = this.files[0],
+        src = file.src,
+        dest = this.data.dest || this.target;
+    grunt.file.write(dest, JSON.stringify(src));
+  });
+
+  // Register task for testing dest
+  grunt.registerMultiTask('echo-dest', 'Save dest to src file', function () {
+    var file = this.files[0],
+        dest = file.dest,
+        src = this.data.src || this.data;
+    grunt.file.write(src, JSON.stringify(dest));
+  });
+
   // Run project task then tests.
   // TEST: We can actually run single string of queries
   // grunt.registerTask('default', 'test-setup 0.4-test nodeunit');
-  grunt.registerTask('default', 'test-setup nodeunit');
+  // grunt.registerTask('default', 'test-setup nodeunit');
+  grunt.registerTask('default', ['echo-dest', 'nodeunit']);
 };
